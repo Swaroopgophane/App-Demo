6750fd2dd4446231d9e51ffe480bc3a0f7ac01ec
Swaroopgophane <swaroop.gophane2017@gmail.com>
2022-10-15 09:57:19 +0530

    app demo

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+
+var Slider = document.getElementById('slider');
+var sliderBtn = document.getElementById('sliderbtn');
+var sliderClose = document.getElementById('sliderclosebtn');
+
+sliderBtn.onclick = function()
+{
+    Slider.style.left = "-350px";
+
+    if(Slider.style.left == "-350px")
+    {
+        Slider.style.left = "0px"
+        sliderBtn.style.display = "none"
+        sliderClose.style.display ="block"
+    }
+}
+
+sliderClose.onclick = function()
+{
+
+    if(Slider.style.left == "0px")
+    {
+        Slider.style.left = "-350px"
+        sliderBtn.style.display = "block"
+        sliderClose.style.display ="none"
+    }
+}
